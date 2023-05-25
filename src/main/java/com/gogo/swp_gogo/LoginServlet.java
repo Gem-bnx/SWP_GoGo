b900a10982408c54531d06163df1442d2f7b0564
atonishingThu <enjoyTheJourneyat03@gmail.com>
2023-05-25 07:01:34 +0700

    first commit

--- src/main/java/com/gogo/swp_gogo/LoginServlet.java
+++ src/main/java/com/gogo/swp_gogo/LoginServlet.java
@@ -1,22 +1,19 @@
 package com.gogo.swp_gogo;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-
 @WebServlet(name="loginServlet", value = "/GoGoLogin")
 public class LoginServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         System.out.println("hello World");
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
 
     }
 }
